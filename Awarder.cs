26a0b2b586fc1a2c1e3909eba46e6be2631f53bf
Brendon W. <bwherry@midland.k12.ia.us>
2026-01-22 12:58:45 -0600

    Update Awarder.cs

--- Awarder.cs
+++ Awarder.cs
@@ -1,13 +1,18 @@
 ﻿namespace knightmoves;
 public class Awarder
 {
-    public string CalculateAward(int points) {}
-    if (pints > 99) {
-        return "First Place"; 
-    } else if(points > 49, && pints < 100) {
-        return "Second Place";
-    } else {
-        return "Participation Award";
+    public string CalculateAward(int points) {
+        if (points > 99) {
+            return "First Place"; 
+        
+        } else if (points > 49 && points < 100) {
+            return "Second Place";
+        
+        } else {
+            return "Participation Award";
+        }
+        
     }
 }
+
 
